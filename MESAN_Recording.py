728422714cddeca8fcb863d158caccbc31fd1424
devRideg <53932587+devRideg@users.noreply.github.com>
2020-11-10 15:59:40 +0100

    MESAN sampling program + additions to utils
    Sampling program logic completed (UNTESTED). Test_MESAN_RECORDING should verify program performs as expected. Added new funtionality to utils: load_dict, ts_to_datetime, sort_by_list.

--- MESAN_Recording.py
+++ MESAN_Recording.py
@@ -1,4 +1,106 @@
 from METCOMP_utils import *
+import os
+
+
+
+
+def combine_data(old_data, new_data):
+
+    old_stations = list(old_data.keys())
+
+    comb_data = old_data
+    for s in new_data:
+        # If there exist station in new data which does not exist in old_data, skip it.
+        if s not in old_data.keys():
+            print('combine_data() >>> station ' + s + ' not in old data. Skipping station')
+            continue
+
+        # Combine data
+        for ts in new_data[s]['frames']:
+            # Do not overwrite old data.
+            if ts in old_data[s]['frames']:
+                continue
+            comb_data[s]['frames'][ts] = new_data[s]['frames'][ts]
+
+
+        old_stations.remove(s)
+
+    if not old_stations:
+        # All stations in old data have got new data.
+        pass
+    else:
+        print('combine_data() >>> The following stations did not receive new data:')
+        for e in old_stations:
+            print(e)
+
+
+    return comb_data
+
+
+
+
+def split_data(comb_data):
+    # res_data contains a regular data object for each day key.
+    # Content of each key in res_data should be written as a
+    # separate file.
+
+    # Collect all timestamps.
+    timestamps = {}
+    for s in comb_data:
+        for ts in comb_data[s]['frames']:
+            timestamps[ts] = None
+
+    # Get corresponding datetime objects
+    timestamps = list(timestamps.keys())
+    dt_timestamps = []
+    for ts in timestamps:
+        tmp_ts = ts.replace("T", " ").strip('Z')
+        dt_timestamps.append(datetime.strptime(tmp_ts, '%Y-%m-%d %H:%M:%S'))
+
+    # Sort string timestamps chronologically.
+    sorted_ts = sort_by_list(timestamps, dt_timestamps)
+
+
+    # Init day keys.
+    res_data = {}
+    for ts in sorted_ts:
+        day = ts.split('T')[0]
+        res_data[day] = {}
+
+    # Loop over days and init dict structure. Copy station data.
+    prev_day = ''
+    for ts in sorted_ts:
+
+        day = ts.split('T')[0]
+        if day == prev_day:
+            continue
+        else:
+            prev_day = day
+
+        # Add station data for each station and init frames dict.
+        res_data[day] = {}
+        for s in comb_data:
+            res_data[day][s] = {}
+            res_data[day][s]['name'] = comb_data[s]['name']
+            res_data[day][s]['municId'] = comb_data[s]['municId']
+            res_data[day][s]['regionId'] = comb_data[s]['regionId']
+            res_data[day][s]['realLong'] = comb_data[s]['realLong']
+            res_data[day][s]['realLat'] = comb_data[s]['realLat']
+            res_data[day][s]['gridLong'] = comb_data[s]['gridLong']
+            res_data[day][s]['gridLat'] = comb_data[s]['gridLat']
+            res_data[day][s]['frames'] = {}
+            pass
+
+    # Add frames chronologically.
+    for ts in sorted_ts:
+        day = ts.split('T')[0]
+        for s in comb_data:
+            res_data[day][s]['frames'][ts] = comb_data[s]['frames'][ts]
+
+    return res_data
+
+
+
 
 LANTMET_URL = 'https://www.ffe.slu.se/lm/json/DownloadJS.cfm'
 SMHI_URL = 'https://opendata-download-metanalys.smhi.se/api/category/mesan1g/version/2/geotype/point/lon/{lon}/lat/{lat}/data.json'
@@ -9,6 +111,10 @@ stations = get_from_api(LANTMET_URL)
 # Collect current data.
 data = {}
 for station in stations:
+
+    # ONLY FOR DEV PURPOSES!!!!
+    # REMOVE THIS LATER
+    #continue
 
     real_lat = station['wgs84N']
     real_lon = station['wgs84e']
@@ -48,15 +154,74 @@ for station in stations:
     print('Collected MESAN for station: ' + str(station['weatherStationId']) + '.')
 
 print('Size: ' + str(data.__sizeof__()) + ' bytes.')
-quit()
 
 
+#save_dict(data, 'MESAN_Test_Dict5.txt')
+#quit()
+
+
+
+
+# ========== COMBING AND SPLITTING DATA ==========
 # Compare with stored files with similar data and
 # add frames which does not exist in stored data.
-# TODO
+
+# This should be data and not loaded from a file
+# Only doing this during development.
+#data = load_dict('MESAN_Test_Dict4.txt')
+
+
+dir = 'MESAN_RECORDED/'
+
+# Check dir for saved files
+files = []
+for r, d, f in os.walk(dir):
+    for file in f:
+        if '.txt' in file:
+            files.append(file)
+
+
+# If directory empty.
+if not files:
+    print('MESAN_RECORDING: No previous files exists.')
+    res_data = split_data(data)
+    for d in res_data:
+        print('                 Writing MESAN_' + d + '.txt.')
+        save_dict(res_data[d], dir + 'MESAN_' + d + '.txt')
+
+
+# Find latest file.
+# If files overlap, old file will get additional data while a new
+# file is written containing contents for the following day.
+else:
+    print('MESAN_RECORDING: Found previous files.')
+    days = []
+    dt_days = []
+    for file in files:
+        day = file.split('_')[1].split('.')[0]
+        days.append(day)
+
+        dt_days.append(datetime.strptime(day, '%Y-%m-%d'))
+
+    days = sort_by_list(days, dt_days)
+    latest_file = 'MESAN_' + days[-1] + '.txt'
+    print(days)
+
+    print('                 Merging with ' + latest_file)
+    old_data = load_dict(dir + latest_file)
+    #print_dict(old_data)
+    comb_data = combine_data(old_data, data)
+    res_data = split_data(comb_data)
+    for d in res_data:
+        if d != days[-1]:
+            continue
+        print('                 Writing MESAN_' + d + '.txt.')
+        save_dict(res_data[d], dir + 'MESAN_' + d + '.txt')
+
 
 
 # GOAL
+# Filename format: MESAN_YYYY-MM-DD.txt
 # One file for each day containing the following JSON data.
 # Each file should contain frames from T00:00:00Z to T23:00:00Z
 #
--- METCOMP_utils.py
+++ METCOMP_utils.py
@@ -1,6 +1,6 @@
 import json
 import requests
-
+from datetime import datetime
 
 
 
@@ -19,10 +19,50 @@ def print_dict(data):
 # @returns None.
 def save_dict(data, filename):
     try:
-        with open(filename, 'w') as f:
-            f.write(json.dumps(data, indent=4, sort_keys=True))
+        with open(filename, 'w', encoding='utf8') as f:
+            f.write(json.dumps(data, indent=4, sort_keys=False, ensure_ascii=False))
     except OSError:
         print('save_dict() >>> Something went wrong trying to save the file.')
+
+
+
+
+# Load JSON data from file
+# @params filename: Name of file containing JSON data.
+# @returns data: Dict with JSON data.
+def load_dict(filename):
+    with open(filename, 'r') as f:
+        data = json.load(f)
+    return data
+
+
+
+
+# Convert string timestamp to datetime object.
+# @params timestamp: string timestamp (YYYY-MM-DDTHH:MM:SSZ).
+# @returns dt_obj: datetime object of timestamp.
+def ts_to_datetime(timestamp):
+    timestamp = timestamp.strip('Z')
+    dt = timestamp.split('T')
+
+    ymd = dt[0].split('-')
+    ymd = [int(x) for x in ymd]
+
+    hms = dt[1].split(':')
+    hms = [int(x) for x in hms]
+
+    dt_obj = datetime(ymd[0], ymd[1], ymd[2], hms[0], hms[1], hms[2])
+    return dt_obj
+
+
+
+
+# Sorts list X by list X. For example if X contains strings of ints and Y actual ints,
+# X can be sorted by performing the same operations as when sorting Y.
+# @params: X, Y: lists of elements. Y must contain comparable objects.
+# @returns: X sorted by Y.
+def sort_by_list(X, Y):
+    return [x for _, x in sorted(zip(Y, X))]
 
 
 
@@ -50,7 +90,7 @@ def get_from_api(url):
 
 
 
-# Convert historic json LANTMET data to lst of observations.
+# Convert historic json LANTMET data to list of observations.
 # @params data: Dictionary to be saved.
 #         params: optional, parameters to be included. Default is all.
 # @returns Dictionary containing list of measurements.
@@ -60,7 +100,7 @@ def LANTMET_to_lists(data):
     list_data['startTime'] = data[0]['timeMeasured']
     list_data['endTime'] = data[-1]['timeMeasured']
 
-    # Init keys.
+    # Init keys with lists.
     for e in data:
         list_data[e['elementMeasurementTypeId']] = []
 
@@ -72,25 +112,28 @@ def LANTMET_to_lists(data):
 
 
 
-# Convert historic (last 24 hours) json MESAN data to lists.
-# Do not save tmin and tmax that are given once a day.
-# @params data: Dictionary to be saved.
-#         params: optional, parameters to be included. Default is all.
-# @returns Dictionary containing list of measurements.
-def MESAN_to_lists(data):
+
+# Trial function to replace original. Verify points are spaced by 1h.
+def LANTMET_to_lists2(data):
 
     list_data = {'startTime': None, 'endTime': None}
-    list_data['startTime'] = data['timeSeries'][-1]['validTime']
-    list_data['endTime'] = data['timeSeries'][0]['validTime']
+    list_data['startTime'] = data[0]['timeMeasured']
+    list_data['endTime'] = data[-1]['timeMeasured']
 
-    # Init keys.
-    for e in data['timeSeries'][0]['parameters']:
-        list_data[e['name']] = []
+    try:
+        list_data['startTime'] = data[0]['timeMeasured']
+    except KeyError:
+        pass
 
-    # Add observations to list in reverse order (oldest to newest).
-    for i in range(len(data['timeSeries'])-1, 0, -1):
-        for e in data['timeSeries'][i]['parameters']:
-            if e['name'] in list_data:
-                list_data[e['name']].append(e['values'][0])
+    # Init lists with number elements = number of hours between startTime and endTime
+    # [hour0, hour1, hour2, hour3, hour4]
+    # If no measurement for hourX,
 
-    return list_data
+    for e in list_data:
+        pass
+
+        # shift to next elements by newTime - oldTime hours
+
+
+
+
